eb8e6069c1a1f40748a81b991e0d5f6fe73e8f45
xLuanKyrb <alexsandroluanbryk@hotmail.com>
2026-04-22 13:05:17 -0300

    Git Inicial
    Dependencias do MAVEN

---
+++ Projeto/sepse/src/main/java/upa/sepse/SepseApplication.java
@@ -0,0 +1,13 @@
+package upa.sepse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepseApplication.class, args);
+	}
+
+}
---
+++ Projeto/sepse/src/test/java/upa/sepse/SepseApplicationTests.java
@@ -0,0 +1,13 @@
+package upa.sepse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
